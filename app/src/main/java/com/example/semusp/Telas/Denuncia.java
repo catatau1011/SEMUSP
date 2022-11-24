7243464eb005174f41fe6a959f2344f9595968ba
catatau1011 <118605794+catatau1011@users.noreply.github.com>
2022-11-24 16:00:49 -0300

    novas telas de opções

--- app/src/main/java/com/example/semusp/Telas/Denuncia.java
+++ app/src/main/java/com/example/semusp/Telas/Denuncia.java
@@ -2,17 +2,81 @@ package com.example.semusp.Telas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.semusp.Escolha.AguaActivity;
+import com.example.semusp.Escolha.ArActivity;
+import com.example.semusp.Escolha.FaunaActivity;
+import com.example.semusp.Escolha.FloraActivity;
+import com.example.semusp.Escolha.FogoActivity;
+import com.example.semusp.Escolha.RuidoActivity;
+import com.example.semusp.Escolha.SoloActivity;
 import com.example.semusp.R;
 
 public class Denuncia extends AppCompatActivity {
+
+    private Button btAgua,btAr,btFauna,btFlora,btFogo,btRuido,btSolo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_denuncia);
         getSupportActionBar().hide();
+
+
+        btAgua = findViewById(R.id.btAgua);
+        btAr = findViewById(R.id.btAr);
+        btFauna = findViewById(R.id.btFauna);
+        btFlora = findViewById(R.id.btFlora);
+        btFogo = findViewById(R.id.btFogo);
+        btRuido = findViewById(R.id.btRuido);
+        btSolo = findViewById(R.id.btSolo);
+
+        btAgua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Denuncia.this, AguaActivity.class));
+            }
+        });
+        btAr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Denuncia.this, ArActivity.class));
+            }
+        });
+        btFauna.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Denuncia.this, FaunaActivity.class));
+            }
+        });
+        btFlora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Denuncia.this, FloraActivity.class));
+            }
+        });
+        btFogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Denuncia.this, FogoActivity.class));
+            }
+        });
+        btRuido.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Denuncia.this, RuidoActivity.class));
+            }
+        });
+        btSolo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Denuncia.this, SoloActivity.class));
+            }
+        });
 
     }
 }

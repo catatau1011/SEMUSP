aedc7fe575ea745c34c7a203f5c8a822200bfbe2
catatau1011 <118605794+catatau1011@users.noreply.github.com>
2022-12-06 11:27:46 -0300

    novas telas de opções

--- app/src/main/java/com/example/semusp/Activity/MainActivity.java
+++ app/src/main/java/com/example/semusp/Activity/MainActivity.java
@@ -1,17 +1,19 @@
-package com.example.semusp.Activity;
+package com.example.semusp;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.TextView;
 
 
-import com.example.semusp.Denucias.CadastrodaDenuncia_Semma;
-import com.example.semusp.R;
 import com.example.semusp.config.ConfiguraçãoFirebase;
 import com.google.firebase.auth.FirebaseAuth;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
 
 public class MainActivity extends IntroActivity {
     private FirebaseAuth autenticacao;
@@ -53,13 +55,7 @@ public class MainActivity extends IntroActivity {
     public void btCadastrar(View v) {
         startActivity(new Intent(this, CadastroActivity.class));
     }
-    public void btConvidado(View v) {
-        startActivity(new Intent(this, CadastrodaDenuncia_Semma.class));
-        Toast.makeText(MainActivity.this,
-                "Realize o cadastro da sua denuncia!\n Convidado",Toast.LENGTH_SHORT).show();
-    }
     //Aqui é onde manda o usuario pra tela principal
-
     public void verificarUsuarioLogado() {
         autenticacao = ConfiguraçãoFirebase.getFirebaseAutenticacao();
         if (autenticacao.getCurrentUser() != null) {
